d8b8f1862d58827e6c2eafe7cac6af54bad87e30
rodrigo azevedo <razevedocosta@gmail.com>
2024-02-23 09:27:47 -0300

    create dashboard route

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -14,6 +14,7 @@ export default function SideNav() {
           <AcmeLogo />
         </div>
       </Link>
+      
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
         <NavLinks />
         <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
